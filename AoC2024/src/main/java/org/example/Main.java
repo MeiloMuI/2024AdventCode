cd515876d437bc930f0b4f7094ed31bca68c0832
“MeiloMuI” <hxy244198079@outlook.com>
2024-12-09 09:21:48 +0000

    day9 upload first version, want to try string and double pointer

--- AoC2024/src/main/java/org/example/Main.java
+++ AoC2024/src/main/java/org/example/Main.java
@@ -5,12 +5,13 @@ import org.example.day4.QuestionHandlerForDay4;
 import org.example.day5.QuestionHandlerForDay5;
 import org.example.day6.QuestionHandlerForDay6;
 import org.example.day7.QuestionHandlerForDay7;
+import org.example.day8.QuestionHandlerForDay8;
 
 public class Main {
     public static void main(String[] args) {
-        QuestionHandlerForDay7 questionHandler = new QuestionHandlerForDay7();
-        System.out.println(questionHandler.getAnswerOfPart1("data/day7/part1.txt"));
-        System.out.println(questionHandler.getAnswerOfPart2("data/day7/part2.txt"));
-//        System.out.println(questionHandler.getAnswerOfPart2("data/day6/part2.txt"));
+        QuestionHandlerForDay8 questionHandler = new QuestionHandlerForDay8();
+        System.out.println(questionHandler.solvePart1("data/day8/part1.txt"));
+        System.out.println(questionHandler.solvePart2("data/day8/part1.txt"));
+
     }
 }
--- AoC2024/src/main/java/org/example/day8/QuestionHandlerForDay8.java
+++ AoC2024/src/main/java/org/example/day8/QuestionHandlerForDay8.java
@@ -18,6 +18,7 @@ public class QuestionHandlerForDay8 {
             char[] ch = s.toCharArray();
             for(int j = 0; j < ch.length; j++){
                 if(ch[j] != '.'){
+                    // Record the position into the map
                     int[] pos = new int[2];
                     pos[0] = i;
                     pos[1] = j;
@@ -32,6 +33,93 @@ public class QuestionHandlerForDay8 {
 
     public int solvePart1(String filePath){
         List<String> data = fileProcessor.readFile(filePath);
-        
+        boolean[][] visited = new boolean[data.size()][data.get(0).length()];
+        Map<Character, List<int[]>> dataMap = getPositionsFromData(data);
+        int result = 0;
+        for(List<int[]> positionsOfSameCharacter: dataMap.values()){
+            int n = positionsOfSameCharacter.size();
+            for(int i = 0; i < n; i++){
+                int[] pos1 = positionsOfSameCharacter.get(i);
+                for(int j = i + 1; j < n; j++){
+                    // Get the distance of gap
+                    int[] pos2 = positionsOfSameCharacter.get(j);
+                    int gap1 = pos2[0] - pos1[0];
+                    int gap2 = pos2[1] - pos1[1];
+                    // pos1 - gap1, gap2 && pos2 + gap1, gap2 (check the boundary before occupied or not)
+                    int[] antinode1 = new int[]{pos1[0] - gap1, pos1[1] - gap2};
+                    int[] antinode2 = new int[]{pos2[0] + gap1, pos2[1] + gap2};
+                    if(checkIsValidPosition(visited, antinode1)){
+                        result++;
+                        visited[antinode1[0]][antinode1[1]] = true;
+                    }
+                    if(checkIsValidPosition(visited, antinode2)){
+                        result++;
+                        visited[antinode2[0]][antinode2[1]] = true;
+                    }
+                }
+            }
+        }
+        return result;
     }
+
+    public int solvePart2(String filePath){
+        List<String> data = fileProcessor.readFile(filePath);
+        boolean[][] visited = new boolean[data.size()][data.get(0).length()];
+        Map<Character, List<int[]>> dataMap = getPositionsFromData(data);
+
+        for(List<int[]> positionsOfSameCharacter: dataMap.values()){
+            int n = positionsOfSameCharacter.size();
+            for(int i = 0; i < n; i++){
+                int[] pos1 = positionsOfSameCharacter.get(i);
+                for(int j = i + 1; j < n; j++){
+                    // Get the distance of gap
+                    int[] pos2 = positionsOfSameCharacter.get(j);
+
+                    // pos1 - gap1, gap2 && pos2 + gap1, gap2 (check the boundary before occupied or not)
+                    checkValidPositions(visited, pos1[0], pos1[1], pos2[0], pos2[1]);
+
+                }
+            }
+        }
+
+        int result = 0;
+        for(int i = 0; i < visited.length; i++){
+            for(int j = 0; j < visited[0].length; j++){
+                if(visited[i][j]) result++;
+            }
+        }
+        return result;
+    }
+
+    public void checkValidPositions(boolean[][] occupied, int pos1X, int pos1Y, int pos2X, int pos2Y){
+        int m = occupied.length;
+        int n = occupied[0].length;
+
+        int gap1 = pos2X - pos1X;
+        int gap2 = pos2Y - pos1Y;
+
+        while(pos1X >=0 && pos1X < m && pos1Y >= 0 && pos1Y < n){
+            occupied[pos1X][pos1Y] = true;
+            pos1X -= gap1;
+            pos1Y -= gap2;
+        }
+
+        while(pos2X >=0 && pos2X < m && pos2Y >= 0 && pos2Y < n){
+            occupied[pos2X][pos2Y] = true;
+            pos2X += gap1;
+            pos2Y += gap2;
+        }
+    }
+
+    public boolean checkIsValidPosition(boolean[][] occupied, int[] posForAntinode){
+        int m = occupied.length;
+        int n = occupied[0].length;
+
+        if(posForAntinode[0] < 0 || posForAntinode[0] >= m || posForAntinode[1] < 0 || posForAntinode[1] >= n){
+            return false;
+        }
+
+        return !occupied[posForAntinode[0]][posForAntinode[1]];
+    }
+
 }
