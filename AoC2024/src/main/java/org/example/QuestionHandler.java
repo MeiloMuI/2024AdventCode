2a0a8042cab2d349495eea3e073e17db7b147d1e
“MeiloMuI” <hxy244198079@outlook.com>
2024-12-10 08:41:48 +0000

    add interface of questionhandler

---
+++ AoC2024/src/main/java/org/example/QuestionHandler.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public interface QuestionHandler {
+    int solvePart1(String filePath);
+    int solvePart2(String filePath);
+}
---
+++ AoC2024/src/main/java/org/example/day10/QuestionHandlerForDay10.java
@@ -0,0 +1,16 @@
+package org.example.day10;
+
+import org.example.QuestionHandler;
+
+public class QuestionHandlerForDay10 implements QuestionHandler {
+
+    @Override
+    public int solvePart1(String filePath) {
+        return 0;
+    }
+
+    @Override
+    public int solvePart2(String filePath) {
+        return 0;
+    }
+}
