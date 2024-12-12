e14e549e5a826ca7b40a405c5d3a37f776ce1c88
“MeiloMuI” <hxy244198079@outlook.com>
2024-12-12 23:34:26 +0000

    build the initial structure

---
+++ AoC2024/src/main/java/org/example/day12/QuestionHandlerForDay12.java
@@ -0,0 +1,19 @@
+package org.example.day12;
+
+import org.example.util.FileProcessor;
+
+public class QuestionHandlerForDay12 {
+    FileProcessor fileProcessor;
+
+    public QuestionHandlerForDay12() {
+        this.fileProcessor = new FileProcessor();
+    }
+
+    public int solvePart1(String filePath){
+        return 0;
+    }
+
+    public int solvePart2(String filePath){
+        return 0;
+    }
+}
