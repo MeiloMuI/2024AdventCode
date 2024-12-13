2f44339e20a0c88ac2aa062950a818510bf306ab
“MeiloMuI” <hxy244198079@outlook.com>
2024-12-13 19:34:48 +0000

    add functions to parse the data(Get the information we need to solve the question)

--- AoC2024/src/main/java/org/example/day13/QuestionHandlerForDay13.java
+++ AoC2024/src/main/java/org/example/day13/QuestionHandlerForDay13.java
@@ -19,29 +19,60 @@ public class QuestionHandlerForDay13 {
         int i = 0;
         while(i < n){
             // Button A
-
+            Button buttonA = generateButtonFromString(data.get(i));
             i++;
             // Button B
-
+            Button buttonB = generateButtonFromString(data.get(i));
             i++;
             // Prize
-
+            Prize prize = generatePrizeFromString(data.get(i));
             i++;
             // Skip one line
+            Machine machine = new Machine(prize, buttonA, buttonB);
+            lists.add(machine);
             i++;
         }
-
         return lists;
     }
 
+    private Prize generatePrizeFromString(String s){
+        String[] arr = s.split(":");
+        String[] prizes = arr[1].split(",");
+        int[] valueOfPrizes = new int[2];
+        for(int i = 0; i < prizes.length; i++){
+            String[] temp = prizes[i].split("=");
+            valueOfPrizes[i] = Integer.parseInt(temp[1]);
+        }
+        return new Prize(valueOfPrizes[0], valueOfPrizes[1]);
+    }
+
     private Button generateButtonFromString(String s){
-        
+        String[] arr = s.split(":");
+        String[] params = arr[1].split(",");
+        int[] values = new int[2];
+        for(int i = 0; i < params.length; i++){
+            String[] temp = params[i].split("\\+");
+            values[i] = Integer.parseInt(temp[1]);
+        }
+        int numOfTokens = 0;
+        if(s.contains("A")){
+            numOfTokens = 3;
+        } else {
+            numOfTokens = 1;
+        }
+        return new Button(values[0], values[1], numOfTokens);
     }
 
     public int solvePart1(String filePath){
         List<String> data = fileProcessor.readFile(filePath);
-
-
+        List<Machine> machines = parseData(data);
+        // Test
+        for(Machine machine: machines){
+            System.out.println("Button A:" + machine.getButtonA().getX());
+            System.out.println("Button B:" + machine.getButtonB().getY());
+            System.out.println("Prize: X" + machine.getPrize().prizeForX + " Y" + machine.getPrize().prizeForY);
+            System.out.println("-----------------");
+        }
         return 0;
     }
 
